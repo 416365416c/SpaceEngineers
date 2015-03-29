41860241f27d83d43492b698d3b2b5366366bc0a
Alan Alpert <416365416c@gmail.com>
2015-03-29 10:40:53 -0700

    Color code CargoPercent
    Also print some more text to help
    diagnose. Given the small amount of
    info, I advise setting the text panel
    to text size 3.0.

--- InventoryManagement/InventoryManagement/CargoPercent.cs
+++ InventoryManagement/InventoryManagement/CargoPercent.cs
@@ -18,44 +18,59 @@ namespace InventoryManagement
 
         #region CodeEditor
         String lcdname = "Cargo Status";
-
-        List<IMyTerminalBlock> containers = new List<IMyTerminalBlock>();
-        IMyTextPanel lcd;
-
-        long maxvol = 0;
-        long curvol = 0;
-
-        void Main()
-        {
-            maxvol = 0;
-            curvol = 0;
-            initBlocks();
-
-            for (int i = 0; i < containers.Count; i++)
-            {
-                IMyCargoContainer container = (IMyCargoContainer)containers[i];
-
-                    maxvol += (long)container.GetInventory(0).MaxVolume;
-                    curvol += (long)container.GetInventory(0).CurrentVolume;
-
+        long warnpercent = 80; 
+ 
+        List<IMyTerminalBlock> containers = new List<IMyTerminalBlock>(); 
+        IMyTextPanel lcd; 
+ 
+        long maxvol = 0; 
+        long curvol = 0; 
+ 
+        void Main() 
+        { 
+            maxvol = 0; 
+            curvol = 0; 
+            initBlocks(); 
+ 
+            for (int i = 0; i < containers.Count; i++) 
+            { 
+                IMyCargoContainer container = (IMyCargoContainer)containers[i]; 
+ 
+                    maxvol += (long)container.GetInventory(0).MaxVolume; 
+                    curvol += (long)container.GetInventory(0).CurrentVolume; 
+ 
             }
-            if (maxvol > 0)
-            {
-                lcd.WritePublicText("Storage: " + ((curvol * 100) / maxvol) + "%", false);
-            }
-            else
-            {
-                lcd.WritePublicText("Storage: 0%", false);
-            }
-
-        }
-
-        void initBlocks()
-        {
-            containers.Clear();
-            GridTerminalSystem.GetBlocksOfType<IMyCargoContainer>(containers);
-            if (lcd == null)
-                lcd = (IMyTextPanel)GridTerminalSystem.GetBlockWithName(lcdname);
+            if (maxvol > 0) 
+            { 
+                long percent = ((curvol * 100) / maxvol); 
+                String output = "Storage:" + percent + "%"; 
+                //Note intent is to fit it on one screen with font size 3.0 
+                output += "\nLeft: " + (maxvol - curvol) + "L"; 
+                if (percent > 99.999) { //100 minus epsilon 
+                    lcd.SetValue<Color>("FontColor", VRageMath.Color.Red); 
+                    output += "\nCARGO FULL"; 
+                } else if (percent > warnpercent) { 
+                    lcd.SetValue<Color>("FontColor", VRageMath.Color.Yellow); 
+                    output += "\nNEAR FULL"; 
+                } else { 
+                    lcd.SetValue<Color>("FontColor", VRageMath.Color.Green); 
+                } 
+                lcd.WritePublicText(output, false); 
+            } 
+            else 
+            { 
+                lcd.SetValue<Color>("FontColor", VRageMath.Color.Red); 
+                lcd.WritePublicText("Storage: 0%\nFound no\ncontainers", false); 
+            } 
+ 
+        } 
+ 
+        void initBlocks() 
+        { 
+            containers.Clear(); 
+            GridTerminalSystem.GetBlocksOfType<IMyCargoContainer>(containers); 
+            if (lcd == null) 
+                lcd = (IMyTextPanel)GridTerminalSystem.GetBlockWithName(lcdname); 
         }
         #endregion
     }
