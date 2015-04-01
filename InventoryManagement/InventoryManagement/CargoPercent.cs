72c0dd80cc72573b0c9c865f8694770b5d087856
Alan Alpert <416365416c@gmail.com>
2015-03-31 20:54:17 -0700

    Adapt to a wide variety of screens
    Automatically adjust size and format to wide screens.
    
    Also make size and color easy to disable.

--- InventoryManagement/InventoryManagement/CargoPercent.cs
+++ InventoryManagement/InventoryManagement/CargoPercent.cs
@@ -20,9 +20,11 @@ namespace InventoryManagement
         String lcdname = "Cargo Status";
         long warnpercent = 80;
         long fullpercent = 99;
+        bool autoSize = true; //If true, will automatically select and set a font size
+        bool autoColor = true; //If true, will automatically select and set a font color
  
         List<IMyTerminalBlock> containers = new List<IMyTerminalBlock>(); 
-        IMyTextPanel lcd;
+        List<IMyTerminalBlock> lcds = new List<IMyTerminalBlock>(); 
  
         long maxvol = 0;
         long curvol = 0;
@@ -41,36 +43,61 @@ namespace InventoryManagement
                     curvol += (long)container.GetInventory(0).CurrentVolume; 
  
             }
+            String output = "Storage:\n0%\nN/A";
+            String outputWide = "Storage: 0%\nFound no\ncontainers";
+            VRageMath.Color colorVar = VRageMath.Color.White;
             if (maxvol > 0) 
             { 
+                //Output intent is to fit it on one screen with font size 5.0 
+                //Wide Output intent is to fit it on one wide screen with font size 6.0 
+                //Half for small screens
                 long percent = ((curvol * 100) / maxvol); 
-                String output = "Storage:\n" + percent + "%"; 
-                //Note intent is to fit it on one screen with font size 5.0 
+                output = "Storage:\n" + percent + "%"; 
+                outputWide = "Storage: " + percent + "%"; 
                 if (percent >= fullpercent) {
-                    lcd.SetValue<Color>("FontColor", VRageMath.Color.Red); 
+                    colorVar = VRageMath.Color.Red;
                     output += "\nAll full"; 
+                    outputWide += "\nCARGO FULL"; 
                 } else if (percent >= warnpercent) { 
-                    lcd.SetValue<Color>("FontColor", VRageMath.Color.Yellow); 
+                    colorVar = VRageMath.Color.Yellow; 
                     output += "\nNear full"; 
+                    outputWide += "\nALMOST FULL";
                 } else { 
-                    lcd.SetValue<Color>("FontColor", VRageMath.Color.Green); 
+                    colorVar = VRageMath.Color.Green; 
                 } 
-                lcd.WritePublicText(output, false); 
-            } 
-            else 
-            { 
-                lcd.SetValue<Color>("FontColor", VRageMath.Color.Red); 
-                lcd.WritePublicText("Storage: 0%\nFound no\ncontainers", false); 
-            } 
- 
+            }
+
+            for (var i=0; i < lcds.Count; i++) {
+                IMyTextPanel lcd = lcds[i] as IMyTextPanel;
+                if (lcd != null) {
+                    String subtypeId = getSubtypeId(lcd);
+                    if (autoColor)
+                        lcd.SetValue<Color>("FontColor", colorVar); 
+                    if (subtypeId.Contains("Wide")) {
+                        lcd.WritePublicText(outputWide, false); 
+                        if (autoSize) 
+                            lcd.SetValueFloat("FontSize", 6.0f); 
+                    } else {
+                        lcd.WritePublicText(output, false); 
+                        //Note that small blocks don't need a small size
+                        if (autoSize)
+                            lcd.SetValueFloat("FontSize", 5.0f); 
+                    }
+                }
+            }
         } 
  
-        void initBlocks() 
-        { 
-            containers.Clear(); 
-            GridTerminalSystem.GetBlocksOfType<IMyCargoContainer>(containers); 
-            if (lcd == null) 
-                lcd = (IMyTextPanel)GridTerminalSystem.GetBlockWithName(lcdname); 
+        String getSubtypeId(IMyTerminalBlock block) {
+            String[] parts = block.BlockDefinition.ToString().Split('/');
+            return parts[parts.Length - 1];
+        }
+
+        void initBlocks()
+        {
+            containers.Clear();
+            GridTerminalSystem.GetBlocksOfType<IMyCargoContainer>(containers);
+            lcds.Clear();
+            GridTerminalSystem.SearchBlocksOfName(lcdname, lcds);
         }
         #endregion
     }
